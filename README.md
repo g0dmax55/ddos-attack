d9f11829d6a2ddd24ed093d1154396046853066e
keralahacker <64751167+keralahacker@users.noreply.github.com>
2020-05-18 10:57:06 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,46 @@
+# DDos-Attack ------- Hack with Vyshu / keralahacker|
+#---------------------------------------------------|
+
+### What Is A DDos-Attack
+
+### A Distributed Denial of Service (DDoS) attack is an attempt to make an online service unavailable 
+by overwhelming it with traffic from multiple sources. They target a wide variety of important resources
+from banks to news websites, and present a major challenge to making sure people can publish and access important information
+
+### kali-linux open terminal an follow the below command 
+
+### Dwonload&Install commands below
+
+### git clone https://github.com/keralahacker/ddos-attack.git
+
+### cd ddos-attack
+
+### chmod +x install.sh
+
+### ./install.sh 
+---------------------------------------------|
+---------------------------------------------|
+### For ANDROID 
+
+### Install TERUMX ==> https://play.google.com/store/apps/details?id=com.termux 
+
+### Dwonload&Install
+
+### https://github.com/keralahacker/ddos-attack.git
+
+### cd ddos-attack
+
+### chmod +x install.sh
+
+### ./install.sh
+
+### YouTube channel
+
+https://www.youtube.com/channel/UCBshYeasyhn3COZHtAe9yNg
+
+### Video Tutorial
+
+ will be updated soon.........!!!!!!!!!!
+
+
+###-----------------------------------------------------||
---
+++ install.sh
@@ -0,0 +1,62 @@
+
+
+echo ""
+echo ""
+echo ""
+read -p $'\e[1;32m\e[0m\e[1;32mT H I S - T O O L - F O R - E D U C A T I O N A l - P U R P O S E S - O N L Y ? \e[1;33m\e[0m\e[1;33m (Y/N) : \e[0m' option
+echo""
+echo""
+echo""
+if [[ $option == *'N'* ]]; then
+clear
+exit
+fi
+if [[ $option == *'n'* ]]; then
+clear
+exit
+fi
+
+pkg install figlet -y 
+clear 
+
+apt-get figlet install -y 
+clear 
+
+figlet  DDos-attack 
+sleep 5
+echo -e $'\033[91m  ___________________________________________________'
+echo -e $'\033[91m |  _   _   _   _   _   _   _   _   _   _   _        |'
+echo -e $'\033[91m | / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ / \       |'
+echo -e $'\033[91m |( D | o | w | n | l | o | a | d | i | n | g )      |'
+echo -e $'\033[91m | \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/       |'
+echo -e $'\033[91m |  _   _   _   _   _   _   _   _   _   _   _   _    |'
+echo -e $'\033[91m | / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ / \   |'
+echo -e $'\033[91m |( R | e | q | u | i | r | e | m | e | n | t | s )  |'
+echo -e $'\033[91m | \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/   |'
+echo -e $'\033[91m |___________________________________________________|'
+echo -e $'\e[1;32m\e[0m\e[1;32m Installing \n ======>  python2 \n ======>  git   \e[0m'
+sleep 3
+pkg install python2 -y
+pkg install python2 -y
+
+pkg i git -y
+apt-get install git -y
+
+clear
+echo -e '\e[96m checking for updates |>> please wait  '
+sleep 5
+#git clone  
+
+chmod +x *
+
+figlet cyber-attack
+sleep 3
+echo -e $'\e[1;32m\e[0m\e[1;32m 8==>0   \e[0m'
+sleep 1
+echo -e $'\e[1;32m\e[0m\e[1;32m 8====>3   \e[0m'
+sleep 2
+echo -e $'\e[1;32m\e[0m\e[1;32m 8=======>2   \e[0m'
+sleep 3
+echo -e $'\e[1;32m\e[0m\e[1;32m 8==========>1   \e[0m'
+sleep 1
+python2 ddos.py
